7411e975c2f8b79974b5efb37cd898b6cc93e0be
huaqiwill <3173484026@qq.com>
2025-06-03 18:22:14 +0800

    集成微信登录功能，修改服务接口

--- qiyun-ur/src/main/java/com/ruoyi/ur/service/IAppointmentService.java
+++ qiyun-ur/src/main/java/com/ruoyi/ur/service/IAppointmentService.java
@@ -2,7 +2,7 @@ package com.ruoyi.ur.service;
 
 import com.ruoyi.ur.domain.dto.AppointmentRequest;
 
-public interface AppointmentService {
+public interface IAppointmentService {
     String createAppointment(AppointmentRequest request);
 
     boolean cancelAppointment(String id);
